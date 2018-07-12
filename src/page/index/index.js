00911f01c828d27c0b2bf79bedb6c1350714c04d
CC-Beelzebub <747558515@qq.com>
2018-07-12 10:51:23 +0800

    配置信息html、url、图片、文字处理

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,8 +1,2 @@
 console.log('hello index')
-newFunction();
-
-function newFunction() {
-    var $$ = require('jquery');
-    // $$('body').html('index hello ~~~');
-    console.log($$);
-}
+require('./index.css')
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
 var path = require("path")
 var webpack = require('webpack');
-var ExtractTextPlugin   = require('extract-text-webpack-plugin');
-
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+//环境变量配置，dev、online
+var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+console.log(WEBPACK_ENV)
+// 获取html-webpack-plugin参数的方法 
+var getHtmlConfig = function (name, title) {
+    return {
+        template: './src/view/' + name + '.html',
+        filename: 'view/' + name + '.html',
+        title: title,
+        inject: true,
+        hash: true,
+        chunks: ['common', name]
+    };
+};
 
 var config = module.exports = {
     entry: {
@@ -11,6 +25,7 @@ var config = module.exports = {
     },
     output: {
         path: './dist',
+        publicPath: '/dist',
         filename: 'js/[name].js'
     },
     externals: {
@@ -18,9 +33,18 @@ var config = module.exports = {
     },
     module: {
         loaders: [{
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-        }]
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=100&name=resource/[name].[ext]'
+            },
+            {
+                test: /\.string$/,
+                loader: 'html-loader'
+            }
+        ]
     },
     plugins: [
         // 独立通用模块到js/base.js
@@ -29,8 +53,16 @@ var config = module.exports = {
             filename: 'js/base.js'
         }),
         // 把css单独打包到文件里
-        new ExtractTextPlugin('css/[name].css'),
+        new ExtractTextPlugin("css/[name].css"),
+        new HtmlWebpackPlugin(getHtmlConfig('index', '首页')),
+        new HtmlWebpackPlugin(getHtmlConfig('login', '登录')),
+
 
     ]
 }
+
+if ('dev' === WEBPACK_ENV) {
+    // config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
+}
+
 module.exports = config;
